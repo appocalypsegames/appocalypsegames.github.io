429285127bfdcffdf962feeb53ec861a07f0026f
appocalypsegames <appocalypse333@gmail.com>
2023-04-09 03:40:29 +0200

    a

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+let veces=10;
 function vibrar() {
   var img = document.querySelector(".vibrar");
   img.classList.add("vibrando");
   setTimeout(function(){
     img.classList.remove("vibrando");
   }, 500);
+  veces--;
+  document.getElementById("textoTamago").innerHTML="Click "+veces+" times on the Illuminati egg and discover what is inside.";
+  if(veces <= 0){
+	  document.getElementById("textoTamago").innerHTML="YOU ARE ILLUMINATI.";
+	  img.src = "images/illuminati.png";
+  }
 }
